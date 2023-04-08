fd528b926f16b5d55d1fd852c59c649ad3a452dd
Max Starikov <maxim.starikov@gmail.com>
2023-04-08 21:33:27 +0200

    add new screens

--- app/src/main/java/com/example/tonwalletactivities/MainActivity.kt
+++ app/src/main/java/com/example/tonwalletactivities/MainActivity.kt
@@ -26,10 +26,15 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_words
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navController.navigate(R.id.navigation_words);
     }
 }
---
+++ app/src/main/java/com/example/tonwalletactivities/ui/screen5/Screen5Fragment.kt
@@ -0,0 +1,43 @@
+package com.example.tonwalletactivities.ui.screen5
+
+import com.example.tonwalletactivities.ui.wallet.WalletViewModel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.tonwalletactivities.databinding.FragmentScreen5Binding
+
+class Screen5Fragment : Fragment() {
+
+    private var _binding: FragmentScreen5Binding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(WalletViewModel::class.java)
+
+        _binding = FragmentScreen5Binding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        // val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            // textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
