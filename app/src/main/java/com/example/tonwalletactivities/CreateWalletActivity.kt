b991969b12eae3fc5d7fb952ebb5d332503bda98
Max Starikov <maxim.starikov@gmail.com>
2023-05-13 16:11:36 +0200

    add dummy fullscreen activity

--- app/src/main/java/com/example/tonwalletactivities/CreateWalletActivity.kt
+++ app/src/main/java/com/example/tonwalletactivities/CreateWalletActivity.kt
@@ -3,6 +3,7 @@ package com.example.tonwalletactivities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tonwalletactivities.ui.create_wallet.CreateWalletFragment
+import com.example.tonwalletactivities.ui.create_wallet.StartWalletFragment
 
 class CreateWalletActivity : AppCompatActivity() {
 
@@ -11,7 +12,8 @@ class CreateWalletActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_wallet)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CreateWalletFragment.newInstance())
+                .replace(R.id.container, StartWalletFragment.newInstance())
+                //.replace(R.id.container, CreateWalletFragment.newInstance())
                 .commitNow()
         }
     }
