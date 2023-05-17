1a25c78888b287667858e7e87db241480be276e8
Max Starikov <maxim.starikov@gmail.com>
2023-05-17 19:39:11 +0200

    add qr library

--- app/src/main/java/com/example/tonwalletactivities/ReceiveActivity.kt
+++ app/src/main/java/com/example/tonwalletactivities/ReceiveActivity.kt
@@ -1,11 +1,25 @@
 package com.example.tonwalletactivities
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
 
 class ReceiveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receive)
+
+        try {
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.encodeBitmap("content", BarcodeFormat.QR_CODE, 400, 400)
+            val imageViewQrCode: ImageView = findViewById<View>(R.id.qrCode) as ImageView
+            imageViewQrCode.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+        }
     }
 }
