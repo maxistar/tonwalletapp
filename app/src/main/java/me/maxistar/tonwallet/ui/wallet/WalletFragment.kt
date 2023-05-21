9d5bf7fcc28ac451f726f94429e12c6c8355172f
Max Starikov <maxim.starikov@gmail.com>
2023-05-21 08:33:44 +0200

    add list class

--- app/src/main/java/me/maxistar/tonwallet/ui/wallet/WalletFragment.kt
+++ app/src/main/java/me/maxistar/tonwallet/ui/wallet/WalletFragment.kt
@@ -1,20 +1,24 @@
 package me.maxistar.tonwallet.ui.wallet
 
+import android.R
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.journeyapps.barcodescanner.ScanContract
-import com.journeyapps.barcodescanner.ScanIntentResult
-import com.journeyapps.barcodescanner.ScanOptions
 import me.maxistar.tonwallet.R
 import me.maxistar.tonwallet.ReceiveActivity
 import me.maxistar.tonwallet.SendActivity
+import me.maxistar.tonwallet.model.TransactionItem
+import java.util.Locale
 
 
 class WalletFragment : Fragment() {
@@ -24,6 +28,8 @@ class WalletFragment : Fragment() {
     }
 
     private lateinit var viewModel: WalletViewModel
+
+    val values: Array<TransactionItem>(20) { TransactionItem(11, "comment", "address") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,5 +57,46 @@ class WalletFragment : Fragment() {
 
         return root;
     }
+
+
+    protected class DictionariesAdapter(context: Context?, textViewResourceId: Int) :
+        ArrayAdapter<TransactionItem?>(context, textViewResourceId, values) {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var v = convertView
+            if (v == null) {
+                val vi = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater?
+                val g: ViewGroup? = null
+                v = vi.inflate(R.layout.dictionary_item, g)
+            }
+            val d: DictionaryItem = values.get(position)
+            if (d != null) {
+                var tv = v!!.findViewById<TextView>(R.id.title)
+                tv.setText(d.title)
+                tv = v.findViewById<TextView>(R.id.lang_source)
+                tv.setText(d.source)
+                tv = v.findViewById<TextView>(R.id.lang_destination)
+                tv.setText(d.destination)
+                tv = v.findViewById<TextView>(R.id.words)
+                val formatString: String
+                formatString = if (d.toLearn < 2) {
+                    DStrings.HTML_WORDS_MEANINGS_LEARN_ERRORS_GREEN
+                } else {
+                    DStrings.HTML_WORDS_MEANINGS_LEARN_ERRORS
+                }
+                tv.text = Html.fromHtml(
+                    java.lang.String.format(
+                        Locale.getDefault(),
+                        formatString,
+                        d.words,
+                        d.meanings,
+                        d.toLearn,
+                        d.newWords
+                    )
+                )
+            }
+            return v!!
+        }
+    }
+
 
 }
